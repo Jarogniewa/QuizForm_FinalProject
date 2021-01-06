14eaddc3f223357fbf1fa7781a675bafbacd15ed
Jarogniewa <marta.jeczen@o2.pl>
2021-01-06 23:45:24 +0100

    Controllers

--- src/main/java/pl/sda/QuizForm_FinalProject/controller/MainController.java
+++ src/main/java/pl/sda/QuizForm_FinalProject/controller/MainController.java
@@ -1,7 +1,10 @@
 package pl.sda.QuizForm_FinalProject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.sda.QuizForm_FinalProject.dto.PollDto;
+import pl.sda.QuizForm_FinalProject.dto.QuizDto;
 
 @Controller
 public class MainController {
@@ -9,6 +12,34 @@ public class MainController {
     @GetMapping({"/", "/index"})
     public String mainPage() {
         return "index";
+    }
+
+    @GetMapping(path = "/create-poll")
+    public String createPoll(Model model)
+    {
+        model.addAttribute("poll", new PollDto());
+        return "create-poll";
+    }
+
+    @GetMapping(path = "/create-quiz")
+    public String createQuiz(Model model)
+    {
+        model.addAttribute("quiz", new QuizDto());
+        return "create-quiz";
+    }
+
+    @GetMapping(path = "/poll")
+    public String poll(Model model)
+    {
+        model.addAttribute("poll", new PollDto());
+        return "poll";
+    }
+
+    @GetMapping(path = "/quiz")
+    public String quiz(Model model)
+    {
+        model.addAttribute("quiz", new QuizDto());
+        return "quiz";
     }
 
 }
--- src/main/java/pl/sda/QuizForm_FinalProject/controller/PollController.java
+++ src/main/java/pl/sda/QuizForm_FinalProject/controller/PollController.java
@@ -1,4 +1,32 @@
 package pl.sda.QuizForm_FinalProject.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.sda.QuizForm_FinalProject.dto.PollDto;
+import pl.sda.QuizForm_FinalProject.service.PollService;
+
+@Controller
+@RequiredArgsConstructor
 public class PollController {
+
+    private final PollService pollService;
+
+    @GetMapping("/create-poll")
+    public String createPoll(Model model)
+    {
+        model.addAttribute("poll", new PollDto());
+        return "create-poll";
+    }
+
+    @PostMapping("/create-poll")
+    public String savePoll(@ModelAttribute("poll") PollDto poll)
+    {
+        pollService.save(poll);
+        return "redirect:/index";
+    }
+
 }
